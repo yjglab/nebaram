3e283fc9dfd73f24fcc9d1de2650fea1084850ed
yjglab <yjgdesign@gmail.com>
2023-08-21 17:42:49 +0900

    chore: search engine tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -53,6 +53,10 @@ const LocaleLayout = async ({ children, params: { locale } }: Props) => {
   return (
     <html lang={locale}>
       <NextIntlClientProvider locale={locale} messages={messages}>
+        <meta
+          name="google-site-verification"
+          content="GGll82-lmBw6qvsAu-yNTYUWaFOTfMLdy3NzyF89ils"
+        />
         <link
           rel="canonical"
           hrefLang="en-US"
